f9d2debe67dc623a81ae27fbc01111457a019194
onsel <>
2004-08-28 07:18:37 +0000

    *** empty log message ***

---
+++ com/turquaz/engine/dal/EngDALConnection.java
@@ -0,0 +1,17 @@
+/*
+ * Created on 25.Aðu.2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package com.turquaz.engine.dal;
+
+/**
+ * @author onsel
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class EngDALConnection {
+
+}
