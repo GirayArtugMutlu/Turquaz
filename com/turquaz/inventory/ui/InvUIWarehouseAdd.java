fd6c5d9c2d5beef165efd37a87706894826b7836
cem <>
2004-10-13 14:46:05 +0000

    *** empty log message ***

--- com/turquaz/inventory/ui/InvUIWarehouseAdd.java
+++ com/turquaz/inventory/ui/InvUIWarehouseAdd.java
@@ -1,3 +1,4 @@
+
 package com.turquaz.inventory.ui;
 
 import org.eclipse.swt.widgets.Shell;
@@ -13,8 +14,14 @@ import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.SWT;
 
+
+
+import com.turquaz.engine.ui.component.SecureComposite;
+
+
 import com.turquaz.engine.ui.component.SecureComposite;
 import com.turquaz.inventory.bl.InvBLWarehouseAdd;
+
 
 /**
 * This code was generated using CloudGarden's Jigloo
@@ -51,6 +58,156 @@ public class InvUIWarehouseAdd extends SecureComposite {
 		try {
 			preInitGUI();
 	
+			compInvTransWare = new Composite(this,SWT.NULL);
+			lblInvTransWhCard = new CLabel(compInvTransWare,SWT.NULL);
+			comboInvTransWhCard = new CCombo(compInvTransWare,SWT.NULL);
+			lblInvTransWhFrom = new Label(compInvTransWare,SWT.NULL);
+			comboInvTransWhFrom = new CCombo(compInvTransWare,SWT.NULL);
+			lblInvTransWhTo = new CLabel(compInvTransWare,SWT.NULL);
+			comboInvTransWhTo = new CCombo(compInvTransWare,SWT.NULL);
+			lblInvTransWhAmount = new CLabel(compInvTransWare,SWT.NULL);
+			txtNumInvTransWhAmount = new Text(compInvTransWare,SWT.NULL);
+			lblInvTransWareUnit = new CLabel(compInvTransWare,SWT.NULL);
+			comboInvTransWhUnit = new CCombo(compInvTransWare,SWT.NULL);
+	
+			this.setSize(new org.eclipse.swt.graphics.Point(329,223));
+	
+	
+			GridData lblInvTransWhCardLData = new GridData();
+			lblInvTransWhCardLData.verticalAlignment = GridData.CENTER;
+			lblInvTransWhCardLData.horizontalAlignment = GridData.BEGINNING;
+			lblInvTransWhCardLData.widthHint = -1;
+			lblInvTransWhCardLData.heightHint = -1;
+			lblInvTransWhCardLData.horizontalIndent = 0;
+			lblInvTransWhCardLData.horizontalSpan = 1;
+			lblInvTransWhCardLData.verticalSpan = 1;
+			lblInvTransWhCardLData.grabExcessHorizontalSpace = false;
+			lblInvTransWhCardLData.grabExcessVerticalSpace = false;
+			lblInvTransWhCard.setLayoutData(lblInvTransWhCardLData);
+			lblInvTransWhCard.setText("Inventory Card");
+	
+			GridData comboInvTransWhCardLData = new GridData();
+			comboInvTransWhCardLData.verticalAlignment = GridData.CENTER;
+			comboInvTransWhCardLData.horizontalAlignment = GridData.BEGINNING;
+			comboInvTransWhCardLData.widthHint = -1;
+			comboInvTransWhCardLData.heightHint = -1;
+			comboInvTransWhCardLData.horizontalIndent = 0;
+			comboInvTransWhCardLData.horizontalSpan = 3;
+			comboInvTransWhCardLData.verticalSpan = 1;
+			comboInvTransWhCardLData.grabExcessHorizontalSpace = false;
+			comboInvTransWhCardLData.grabExcessVerticalSpace = false;
+			comboInvTransWhCard.setLayoutData(comboInvTransWhCardLData);
+	
+			GridData lblInvTransWhFromLData = new GridData();
+			lblInvTransWhFromLData.verticalAlignment = GridData.CENTER;
+			lblInvTransWhFromLData.horizontalAlignment = GridData.BEGINNING;
+			lblInvTransWhFromLData.widthHint = -1;
+			lblInvTransWhFromLData.heightHint = -1;
+			lblInvTransWhFromLData.horizontalIndent = 0;
+			lblInvTransWhFromLData.horizontalSpan = 1;
+			lblInvTransWhFromLData.verticalSpan = 1;
+			lblInvTransWhFromLData.grabExcessHorizontalSpace = false;
+			lblInvTransWhFromLData.grabExcessVerticalSpace = false;
+			lblInvTransWhFrom.setLayoutData(lblInvTransWhFromLData);
+			lblInvTransWhFrom.setText("From:");
+	
+			GridData comboInvTransWhFromLData = new GridData();
+			comboInvTransWhFromLData.verticalAlignment = GridData.CENTER;
+			comboInvTransWhFromLData.horizontalAlignment = GridData.BEGINNING;
+			comboInvTransWhFromLData.widthHint = -1;
+			comboInvTransWhFromLData.heightHint = -1;
+			comboInvTransWhFromLData.horizontalIndent = 0;
+			comboInvTransWhFromLData.horizontalSpan = 3;
+			comboInvTransWhFromLData.verticalSpan = 1;
+			comboInvTransWhFromLData.grabExcessHorizontalSpace = false;
+			comboInvTransWhFromLData.grabExcessVerticalSpace = false;
+			comboInvTransWhFrom.setLayoutData(comboInvTransWhFromLData);
+	
+			GridData lblInvTransWhToLData = new GridData();
+			lblInvTransWhToLData.verticalAlignment = GridData.CENTER;
+			lblInvTransWhToLData.horizontalAlignment = GridData.BEGINNING;
+			lblInvTransWhToLData.widthHint = -1;
+			lblInvTransWhToLData.heightHint = -1;
+			lblInvTransWhToLData.horizontalIndent = 0;
+			lblInvTransWhToLData.horizontalSpan = 1;
+			lblInvTransWhToLData.verticalSpan = 1;
+			lblInvTransWhToLData.grabExcessHorizontalSpace = false;
+			lblInvTransWhToLData.grabExcessVerticalSpace = false;
+			lblInvTransWhTo.setLayoutData(lblInvTransWhToLData);
+			lblInvTransWhTo.setText("To:");
+	
+			GridData comboInvTransWhToLData = new GridData();
+			comboInvTransWhToLData.verticalAlignment = GridData.CENTER;
+			comboInvTransWhToLData.horizontalAlignment = GridData.BEGINNING;
+			comboInvTransWhToLData.widthHint = -1;
+			comboInvTransWhToLData.heightHint = -1;
+			comboInvTransWhToLData.horizontalIndent = 0;
+			comboInvTransWhToLData.horizontalSpan = 3;
+			comboInvTransWhToLData.verticalSpan = 1;
+			comboInvTransWhToLData.grabExcessHorizontalSpace = false;
+			comboInvTransWhToLData.grabExcessVerticalSpace = false;
+			comboInvTransWhTo.setLayoutData(comboInvTransWhToLData);
+	
+			GridData lblInvTransWhAmountLData = new GridData();
+			lblInvTransWhAmountLData.verticalAlignment = GridData.CENTER;
+			lblInvTransWhAmountLData.horizontalAlignment = GridData.BEGINNING;
+			lblInvTransWhAmountLData.widthHint = -1;
+			lblInvTransWhAmountLData.heightHint = -1;
+			lblInvTransWhAmountLData.horizontalIndent = 0;
+			lblInvTransWhAmountLData.horizontalSpan = 1;
+			lblInvTransWhAmountLData.verticalSpan = 1;
+			lblInvTransWhAmountLData.grabExcessHorizontalSpace = false;
+			lblInvTransWhAmountLData.grabExcessVerticalSpace = false;
+			lblInvTransWhAmount.setLayoutData(lblInvTransWhAmountLData);
+			lblInvTransWhAmount.setText("Amount");
+	
+			GridData txtNumInvTransWhAmountLData = new GridData();
+			txtNumInvTransWhAmountLData.verticalAlignment = GridData.CENTER;
+			txtNumInvTransWhAmountLData.horizontalAlignment = GridData.BEGINNING;
+			txtNumInvTransWhAmountLData.widthHint = -1;
+			txtNumInvTransWhAmountLData.heightHint = -1;
+			txtNumInvTransWhAmountLData.horizontalIndent = 0;
+			txtNumInvTransWhAmountLData.horizontalSpan = 1;
+			txtNumInvTransWhAmountLData.verticalSpan = 1;
+			txtNumInvTransWhAmountLData.grabExcessHorizontalSpace = false;
+			txtNumInvTransWhAmountLData.grabExcessVerticalSpace = false;
+			txtNumInvTransWhAmount.setLayoutData(txtNumInvTransWhAmountLData);
+	
+			GridData lblInvTransWareUnitLData = new GridData();
+			lblInvTransWareUnitLData.verticalAlignment = GridData.CENTER;
+			lblInvTransWareUnitLData.horizontalAlignment = GridData.BEGINNING;
+			lblInvTransWareUnitLData.widthHint = -1;
+			lblInvTransWareUnitLData.heightHint = -1;
+			lblInvTransWareUnitLData.horizontalIndent = 0;
+			lblInvTransWareUnitLData.horizontalSpan = 1;
+			lblInvTransWareUnitLData.verticalSpan = 1;
+			lblInvTransWareUnitLData.grabExcessHorizontalSpace = false;
+			lblInvTransWareUnitLData.grabExcessVerticalSpace = false;
+			lblInvTransWareUnit.setLayoutData(lblInvTransWareUnitLData);
+			lblInvTransWareUnit.setText("Unit");
+	
+			GridData comboInvTransWhUnitLData = new GridData();
+			comboInvTransWhUnitLData.verticalAlignment = GridData.CENTER;
+			comboInvTransWhUnitLData.horizontalAlignment = GridData.BEGINNING;
+			comboInvTransWhUnitLData.widthHint = 64;
+			comboInvTransWhUnitLData.heightHint = 16;
+			comboInvTransWhUnitLData.horizontalIndent = 0;
+			comboInvTransWhUnitLData.horizontalSpan = 1;
+			comboInvTransWhUnitLData.verticalSpan = 1;
+			comboInvTransWhUnitLData.grabExcessHorizontalSpace = false;
+			comboInvTransWhUnitLData.grabExcessVerticalSpace = false;
+			comboInvTransWhUnit.setLayoutData(comboInvTransWhUnitLData);
+			comboInvTransWhUnit.setSize(new org.eclipse.swt.graphics.Point(64,16));
+			GridLayout compInvTransWareLayout = new GridLayout(4, true);
+			compInvTransWare.setLayout(compInvTransWareLayout);
+			compInvTransWareLayout.marginWidth = 5;
+			compInvTransWareLayout.marginHeight = 5;
+			compInvTransWareLayout.numColumns = 4;
+			compInvTransWareLayout.makeColumnsEqualWidth = true;
+			compInvTransWareLayout.horizontalSpacing = 5;
+			compInvTransWareLayout.verticalSpacing = 5;
+			compInvTransWare.layout();
+			FillLayout thisLayout = new FillLayout(256);
 			lblWarehouseName = new CLabel(this,SWT.NULL);
 			txtWarehouseName = new Text(this,SWT.NULL);
 			lblWarehouseAdres = new CLabel(this,SWT.NULL);
@@ -161,6 +318,7 @@ public class InvUIWarehouseAdd extends SecureComposite {
 	public static void main(String[] args){
 		showGUI();
 	}
+
 	
 	boolean verifyFields(){
 	  return true;
@@ -194,9 +352,7 @@ public class InvUIWarehouseAdd extends SecureComposite {
 	}
 	public void search(){
 	}
-	public void delete(){
-	
-	}
+
 
 	/**
 	* This static method creates a new instance of this class and shows
