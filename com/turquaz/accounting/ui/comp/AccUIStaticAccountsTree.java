5413f19f97c163f4f8032c6fe3e3abc7318a1279
ehad <>
2004-11-04 13:16:30 +0000

    turquaz headers and licence information added

--- com/turquaz/accounting/ui/comp/AccUIStaticAccountsTree.java
+++ com/turquaz/accounting/ui/comp/AccUIStaticAccountsTree.java
@@ -5,6 +5,26 @@
  * Window - Preferences - Java - Code Style - Code Templates
  */
 package com.turquaz.accounting.ui.comp;
+/************************************************************************/
+/* TURQUAZ: Higly Modular Accounting/ERP Program                        */
+/* ============================================                         */
+/* Copyright (c) 2004 by Turquaz Software Development Group			    */
+/*																		*/
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License, or    */
+/* (at your option) any later version.       							*/
+/* 																		*/
+/* This program is distributed in the hope that it will be useful,		*/
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of		*/
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the		*/
+/* GNU General Public License for more details.         				*/
+/************************************************************************/
+
+/**
+* @author  Onsel Armagan
+* @version  $Id$
+*/
 
 import java.util.HashMap;
 import java.util.List;
