921d280ca40770c6e1d51f224e8b9905febcd52c
onsel <>
2004-11-10 12:39:57 +0000

    update consignment dal and bl classes are implemented

---
+++ com/turquaz/consignment/bl/ConBLUpdateConsignment.java
@@ -0,0 +1,72 @@
+
+package com.turquaz.consignment.bl;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
+import com.turquaz.consignment.dal.ConDALUpdateConsignment;
+import com.turquaz.engine.dal.TurqConsignment;
+import com.turquaz.engine.dal.TurqCurrentCard;
+
+/**
+ * @author onsel
+ *
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class ConBLUpdateConsignment {
+	private Calendar cal = Calendar.getInstance();
+	private ConDALUpdateConsignment dalCons = new ConDALUpdateConsignment();
+	
+	public ConBLUpdateConsignment(){
+		
+	}
+	
+	public void updateConsignment(TurqConsignment consignment,
+			   String docNo, String definition, Date consignmentDate,
+			   TurqCurrentCard curCard, int discountRate,BigDecimal discountAmount,
+			   String billDocNo, BigDecimal vatAmount,BigDecimal specialVatAmount,
+			   BigDecimal totalAmount,int type)throws Exception{
+		try{
+		
+		consignment.setCondignmentsDiscountRate(discountRate);
+		consignment.setConsignmentsBillDocumentNo(billDocNo);
+		consignment.setConsignmentsCharges(new BigDecimal(0));
+		consignment.setConsignmentsDate(consignmentDate);
+		consignment.setConsignmentsDefinition(definition);
+		consignment.setConsignmentsDiscountAmount(discountAmount);
+		consignment.setConsignmentsDocumentNo(docNo);
+		consignment.setConsignmentsTotalAmount(totalAmount);
+		consignment.setConsignmentsType(type);
+		consignment.setConsignmentsVatAmount(vatAmount);
+		consignment.setConsignmentsSpecialVatAmount(specialVatAmount);
+		consignment.setTurqCurrentCard(curCard);		
+		consignment.setUpdatedBy(System.getProperty("user"));
+		consignment.setLastModified(new java.sql.Date(cal.getTime().getTime()));
+		dalCons.updateObject(consignment);
+		
+		
+		}
+		catch(Exception ex){
+			throw ex;
+		}
+	
+	
+	}
+	
+	public void deleteObject(Object obj)throws Exception{
+	try{
+		
+		dalCons.deleteObject(obj);
+	
+	}
+	catch(Exception ex){
+			throw ex;
+	
+	}
+	}
+	
+	
+	
+	
+
+}
