710c6137c1b5ab60fbf249b740463e17b8826139
onsel <>
2004-11-18 21:42:23 +0000

    *** empty log message ***

--- com/turquaz/accounting/ui/AccUIAccountingPlan.java
+++ com/turquaz/accounting/ui/AccUIAccountingPlan.java
@@ -4,7 +4,7 @@ package com.turquaz.accounting.ui;
 /* ============================================                         */
 /* Copyright (c) 2004 by Turquaz Software Development Group			    */
 /*																		*/
-/* This program is free software. You can redistribute it and/or modify */
+/* This program is free so ftware. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
 /* the Free Software Foundation; either version 2 of the License, or    */
 /* (at your option) any later version.       							*/
