643b7df4f710f4fbedc27baa100477920f60f8e2
ehad <>
2004-11-19 20:40:30 +0000

    tree permissions updated

--- com/turquaz/engine/ui/component/TreeFactory.java
+++ com/turquaz/engine/ui/component/TreeFactory.java
@@ -131,102 +131,160 @@ public final class TreeFactory {
 		item.setText(com.turquaz.engine.Messages.getString("TreeFactory.10")); //$NON-NLS-1$
 		item.setData(AccUIAddAccounts.class.getName());
 		}
+		if(EngBLPermissions.getPermission(AccUIAccountingPlan.class.getName())>0){
 		item = new TreeItem(root,SWT.NULL);
 		item.setText(com.turquaz.engine.Messages.getString("TreeFactory.11")); //$NON-NLS-1$
 		item.setData(AccUIAccountingPlan.class.getName());
+		}
+		if(EngBLPermissions.getPermission(AccUITransactionAdd.class.getName())>0){
 		item = new TreeItem(root,SWT.NULL);
 		item.setText(com.turquaz.engine.Messages.getString("TreeFactory.12")); //$NON-NLS-1$
 		item.setData(AccUITransactionAdd.class.getName());	
+		}
+		if(EngBLPermissions.getPermission(AccUITransactionCollect.class.getName())>0){
 		item = new TreeItem(root,SWT.NULL);
 		item.setText(com.turquaz.engine.Messages.getString("TreeFactory.13")); //$NON-NLS-1$
 		item.setData(AccUITransactionCollect.class.getName());	
+		}
+		if(EngBLPermissions.getPermission(AccUITransactionPayment.class.getName())>0){
 		item = new TreeItem(root,SWT.NULL);
 		item.setText(com.turquaz.engine.Messages.getString("TreeFactory.14")); //$NON-NLS-1$
 		item.setData(AccUITransactionPayment.class.getName());	
+		}
+		if(EngBLPermissions.getPermission(AccUITransactionSearch.class.getName())>0){
 		item = new TreeItem(root,SWT.NULL);
 		item.setText(com.turquaz.engine.Messages.getString("TreeFactory.15")); //$NON-NLS-1$
 		item.setData(AccUITransactionSearch.class.getName());
+		}
 		
 		TreeItem report = new TreeItem(tree,SWT.NULL);
 		report.setText(Messages.getString("TreeFactory.32")); //$NON-NLS-1$
 		
+		if(EngBLPermissions.getPermission(AccUIAccountingJournal.class.getName())>0){
 		item = new TreeItem(report,SWT.NULL);
 		item.setText(Messages.getString("TreeFactory.28")); //$NON-NLS-1$
 		item.setData(AccUIAccountingJournal.class.getName());
+		}
+		
+		if(EngBLPermissions.getPermission(AccUIAccountingBalance.class.getName())>0){
 		item = new TreeItem(report,SWT.NULL);
 		item.setText(Messages.getString("TreeFactory.29")); //$NON-NLS-1$
 		item.setData(AccUIAccountingBalance.class.getName());
+		}
+		
+		if(EngBLPermissions.getPermission(AccUIAccountingGeneralLedger.class.getName())>0){
 		item = new TreeItem(report,SWT.NULL);
 		item.setText(Messages.getString("TreeFactory.33")); //$NON-NLS-1$
 		item.setData(AccUIAccountingGeneralLedger.class.getName());
-		
+		}
 		
 		return tree;
 	}
 	public static Tree createCurrentTree(Tree tree){
 		TreeItem root = new TreeItem(tree,SWT.NULL);
 		root.setText(com.turquaz.engine.Messages.getString("TreeFactory.16")); //$NON-NLS-1$
-		TreeItem item = new TreeItem(root,SWT.NULL);
+		
+		TreeItem item;
+		if(EngBLPermissions.getPermission(CurUICurrentCardAdd.class.getName())>0){
+		item = new TreeItem(root,SWT.NULL);
 		item.setText(com.turquaz.engine.Messages.getString("TreeFactory.17")); //$NON-NLS-1$
 		item.setData(CurUICurrentCardAdd.class.getName());
+		}
+		if(EngBLPermissions.getPermission(CurUICurrentCardSearch.class.getName())>0){
 		item = new TreeItem(root,SWT.NULL);
 		item.setText(com.turquaz.engine.Messages.getString("TreeFactory.18")); //$NON-NLS-1$
 		item.setData(CurUICurrentCardSearch.class.getName());
+		}
+		if(EngBLPermissions.getPermission(CurUITransactionAdd.class.getName())>0){
 		item = new TreeItem(root,SWT.NULL);
 		item.setText(com.turquaz.engine.Messages.getString("TreeFactory.19")); //$NON-NLS-1$
 		item.setData(CurUITransactionAdd.class.getName());
+		}
+		if(EngBLPermissions.getPermission(CurUITransactionSearch.class.getName())>0){
 		item = new TreeItem(root,SWT.NULL);
 		item.setText(com.turquaz.engine.Messages.getString("TreeFactory.20")); //$NON-NLS-1$
 		item.setData(CurUITransactionSearch.class.getName());
+		}
+		
 		return tree;
 	}
 	public static Tree createAdminTree(Tree tree){
 		TreeItem root = new TreeItem(tree,SWT.NULL);
 		root.setText(com.turquaz.engine.Messages.getString("TreeFactory.21")); //$NON-NLS-1$
-		TreeItem item = new TreeItem(root,SWT.NULL);
+		
+		TreeItem item;
+		
+		if(EngBLPermissions.getPermission(AdmUIUserAdd.class.getName())>0){
+		item = new TreeItem(root,SWT.NULL);
 		item.setText(Messages.getString("TreeFactory.23")); //$NON-NLS-1$
 		item.setData(AdmUIUserAdd.class.getName());
+		}
+		if(EngBLPermissions.getPermission(AdmUIUsers.class.getName())>0){
 		item = new TreeItem(root,SWT.NULL);
 		item.setText(Messages.getString("TreeFactory.24")); //$NON-NLS-1$
 		item.setData(AdmUIUsers.class.getName());
+		}
+		if(EngBLPermissions.getPermission(AdmUIGroupAdd.class.getName())>0){
 		item = new TreeItem(root,SWT.NULL);
 		item.setText(Messages.getString("TreeFactory.25")); //$NON-NLS-1$
 		item.setData(AdmUIGroupAdd.class.getName());
+		}
+		if(EngBLPermissions.getPermission(AdmUIGroups.class.getName())>0){
 		item = new TreeItem(root,SWT.NULL);
 		item.setText(Messages.getString("TreeFactory.26")); //$NON-NLS-1$
 		item.setData(AdmUIGroups.class.getName());
+		}
+		if(EngBLPermissions.getPermission(AdmUIUserPermissions.class.getName())>0){
 		item = new TreeItem(root,SWT.NULL);
 		item.setText(Messages.getString("TreeFactory.27")); //$NON-NLS-1$
 		item.setData(AdmUIUserPermissions.class.getName());
+		}
+		if(EngBLPermissions.getPermission(AdmUIGroupPermissions.class.getName())>0){
 		item = new TreeItem(root,SWT.NULL);
 		item.setText(Messages.getString("TreeFactory.22")); //$NON-NLS-1$
 		item.setData(AdmUIGroupPermissions.class.getName());
+		}
 		return tree;
 	}
 	public static Tree createConsignmetTree(Tree tree){
 		TreeItem root = new TreeItem(tree,SWT.NULL);
 		root.setText(Messages.getString("TreeFactory.31")); //$NON-NLS-1$
-		TreeItem item = new TreeItem(root,SWT.NULL);
+		
+		TreeItem item;
+		if(EngBLPermissions.getPermission(ConUIAddConsignment.class.getName())>0){
+		item = new TreeItem(root,SWT.NULL);
 		item.setText(Messages.getString("TreeFactory.30")); //$NON-NLS-1$
 		item.setData(ConUIAddConsignment.class.getName());
+		}
+		if(EngBLPermissions.getPermission(ConUIConsignmentSearch.class.getName())>0){
 		item = new TreeItem(root,SWT.NULL);
 		item.setText(Messages.getString("TreeFactory.34")); //$NON-NLS-1$
 		item.setData(ConUIConsignmentSearch.class.getName());
+		}
 		return tree;
 		
 	}
 	public static Tree createBillTree(Tree tree){
 		TreeItem root = new TreeItem(tree,SWT.NULL);
 		root.setText(Messages.getString("TreeFactory.36")); //$NON-NLS-1$
-		TreeItem item = new TreeItem(root,SWT.NULL);
+		
+		TreeItem item;
+		
+		if(EngBLPermissions.getPermission(BillUIBillFromConsignment.class.getName())>0){
+		item = new TreeItem(root,SWT.NULL);
 		item.setText(Messages.getString("TreeFactory.37")); //$NON-NLS-1$
 		item.setData(BillUIBillFromConsignment.class.getName());
-	    item = new TreeItem(root,SWT.NULL);
+		}
+	    if(EngBLPermissions.getPermission(BillUIBillSearch.class.getName())>0){
+		item = new TreeItem(root,SWT.NULL);
 		item.setText(Messages.getString("TreeFactory.2")); //$NON-NLS-1$
 		item.setData(BillUIBillSearch.class.getName());
+	    }
+		if(EngBLPermissions.getPermission(BillUIAddBill.class.getName())>0){
 		item = new TreeItem(root,SWT.NULL);
 		item.setText(Messages.getString("TreeFactory.38")); //$NON-NLS-1$
 		item.setData(BillUIAddBill.class.getName());
+		}
 		return tree;
 		
 	}
