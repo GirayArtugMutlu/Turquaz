adcf44391f87a3d4e31b4f74ccfcb58aebbe6631
onsel <>
2004-11-25 17:16:11 +0000

    .cvignores added

--- com/cloudgarden/resource/SWTResourceManager.java
+++ com/cloudgarden/resource/SWTResourceManager.java
@@ -1,3 +1,4 @@
+
 package com.cloudgarden.resource;
 
 import java.util.HashMap;
--- com/turquaz/accounting/ui/AccUIInitialTransaction.java
+++ com/turquaz/accounting/ui/AccUIInitialTransaction.java
@@ -1,10 +1,29 @@
 package com.turquaz.accounting.ui;
 
+import java.math.BigDecimal;
+
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.layout.GridData;
+
+import com.turquaz.engine.dal.TurqAccountingAccount;
+import com.turquaz.engine.dal.TurqAccountingTransaction;
+import com.turquaz.engine.dal.TurqAccountingTransactionColumn;
+import com.turquaz.engine.ui.component.DecimalText;
+import org.eclipse.swt.custom.CCombo;
+import com.turquaz.engine.ui.component.SecureComposite;
+
+import org.eclipse.swt.widgets.Text;
+import com.cloudgarden.resource.SWTResourceManager;
+import com.turquaz.engine.ui.component.DatePicker;
+import com.turquaz.accounting.Messages;
+import com.turquaz.accounting.bl.AccBLTransactionAdd;
+import com.turquaz.accounting.ui.comp.AccountPicker;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.SWT;
 
@@ -23,7 +42,27 @@ import org.eclipse.swt.SWT;
 * for any corporate or commercial purpose.
 * *************************************
 */
-public class AccUIInitialTransaction extends org.eclipse.swt.widgets.Composite {
+public class AccUIInitialTransaction extends org.eclipse.swt.widgets.Composite implements SecureComposite {
+
+    {
+        //Register as a resource user - SWTResourceManager will
+        //handle the obtaining and disposing of resources
+        SWTResourceManager.registerResourceUser(this);
+    }
+
+	private CLabel lblAccount;
+	private CLabel lblDocumentNo;
+	private Text txtDefinition;
+	private CCombo comboDeptOrCredit;
+	private CLabel lbDeptOrCredit;
+	private CLabel lblDefinition;
+	private Text txtDocumentNo;
+	private DatePicker datePicker;
+	private CLabel lblDate;
+	private DecimalText decInitialValue;
+	private CLabel lblInitialValue;
+	private AccountPicker accPicker;
+	private AccBLTransactionAdd blTrans = new AccBLTransactionAdd();
 
 	/**
 	* Auto-generated main method to display this 
@@ -68,12 +107,149 @@ public class AccUIInitialTransaction extends org.eclipse.swt.widgets.Composite {
 
 	private void initGUI() {
 		try {
-			this.setLayout(new GridLayout());
-			this.setSize(462, 168);
+			GridLayout thisLayout = new GridLayout();
+			this.setLayout(thisLayout);
+			thisLayout.numColumns = 2;
+			this.setSize(454, 216);
+            {
+                lblAccount = new CLabel(this, SWT.NONE);
+                lblAccount.setText(Messages.getString("AccUIInitialTransaction.0")); //$NON-NLS-1$
+                GridData lblAccountLData = new GridData();
+                lblAccountLData.widthHint = 118;
+                lblAccountLData.heightHint = 22;
+                lblAccount.setLayoutData(lblAccountLData);
+            }
+            {
+                accPicker = new AccountPicker(this, SWT.NONE);
+                GridData accPickerLData = new GridData();
+                accPickerLData.heightHint = 23;
+                accPickerLData.horizontalAlignment = GridData.FILL;
+                accPicker.setLayoutData(accPickerLData);
+            }
+            {
+                lblInitialValue = new CLabel(this, SWT.NONE);
+                lblInitialValue.setText(Messages.getString("AccUIInitialTransaction.1")); //$NON-NLS-1$
+                GridData lblInitialValueLData = new GridData();
+                lblInitialValueLData.widthHint = 92;
+                lblInitialValueLData.heightHint = 18;
+                lblInitialValue.setLayoutData(lblInitialValueLData);
+            }
+            {
+                decInitialValue = new DecimalText(this, SWT.NONE);
+                GridData decInitialValueLData = new GridData();
+                decInitialValueLData.horizontalAlignment = GridData.FILL;
+                decInitialValueLData.heightHint = 19;
+                decInitialValue.setLayoutData(decInitialValueLData);
+            }
+            {
+                lblDate = new CLabel(this, SWT.NONE);
+                lblDate.setText(Messages.getString("AccUIInitialTransaction.2")); //$NON-NLS-1$
+            }
+            {
+                datePicker = new DatePicker(this, SWT.NONE);
+                GridData datePickerLData = new GridData();
+                datePicker.setFont(SWTResourceManager.getFont(Messages.getString("AccUIInitialTransaction.3"), 8, 1, false, false)); //$NON-NLS-1$
+                datePickerLData.widthHint = 165;
+                datePickerLData.heightHint = 25;
+                datePicker.setLayoutData(datePickerLData);
+            }
+            {
+                lblDocumentNo = new CLabel(this, SWT.NONE);
+                lblDocumentNo.setText(Messages.getString("AccUIInitialTransaction.4")); //$NON-NLS-1$
+            }
+            {
+                txtDocumentNo = new Text(this, SWT.NONE);
+                GridData txtDocumentNoLData = new GridData();
+                txtDocumentNoLData.horizontalAlignment = GridData.FILL;
+                txtDocumentNoLData.heightHint = 19;
+                txtDocumentNo.setLayoutData(txtDocumentNoLData);
+            }
+            {
+                lblDefinition = new CLabel(this, SWT.NONE);
+                lblDefinition.setText(Messages.getString("AccUIInitialTransaction.5")); //$NON-NLS-1$
+                GridData lblDefinitionLData = new GridData();
+                lblDefinitionLData.widthHint = 86;
+                lblDefinitionLData.heightHint = 20;
+                lblDefinitionLData.verticalAlignment = GridData.BEGINNING;
+                lblDefinition.setLayoutData(lblDefinitionLData);
+            }
+            {
+                txtDefinition = new Text(this, SWT.MULTI | SWT.V_SCROLL);
+                GridData txtDefinitionLData = new GridData();
+                txtDefinitionLData.widthHint = 237;
+                txtDefinitionLData.heightHint = 52;
+                txtDefinition.setLayoutData(txtDefinitionLData);
+            }
+            {
+                lbDeptOrCredit = new CLabel(this, SWT.NONE);
+                lbDeptOrCredit.setText(Messages
+                    .getString("AccUITransactionRowAddDialog.1"));
+                GridData lbDeptOrCreditLData = new GridData();
+                lbDeptOrCreditLData.widthHint = 76;
+                lbDeptOrCreditLData.heightHint = 18;
+                lbDeptOrCredit.setLayoutData(lbDeptOrCreditLData);
+            }
+            {
+                comboDeptOrCredit = new CCombo(this, SWT.READ_ONLY);
+                comboDeptOrCredit.setText(Messages
+                    .getString("AccUITransactionRowAddDialog.6"));
+                comboDeptOrCredit.setBackground(SWTResourceManager.getColor(
+                    255,
+                    255,
+                    255));
+                comboDeptOrCredit.setSize(new org.eclipse.swt.graphics.Point(
+                    71,
+                    17));
+                GridData comboDeptOrCreditLData = new GridData();
+                comboDeptOrCreditLData.widthHint = 49;
+                comboDeptOrCreditLData.heightHint = 17;
+                comboDeptOrCredit.setLayoutData(comboDeptOrCreditLData);
+            }
 			this.layout();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	public void search(){
+	    
+	}
+	public void save(){
+	    MessageBox msg = new MessageBox(this.getShell(),SWT.NULL);
+	    
+	    try{
+	       Integer id = blTrans.saveAccTransaction(datePicker.getDate(),txtDocumentNo.getText().trim(),3,1,null,txtDefinition.getText().trim());
+	       TurqAccountingTransactionColumn transRow = new TurqAccountingTransactionColumn();
+	      
+	      transRow.setTurqAccountingAccount((TurqAccountingAccount)accPicker.getData());	
+	   	
+	   	if(comboDeptOrCredit.getText().equals(Messages.getString("AccUITransactionRowAddDialog.6"))){ //$NON-NLS-1$
+	   	transRow.setCreditAmount(new BigDecimal(0));
+	    transRow.setDeptAmount(decInitialValue.getBigDecimalValue());
+	    }
+	   	
+	   	else {
+	   	transRow.setDeptAmount(new BigDecimal(0));
+	   	transRow.setCreditAmount(decInitialValue.getBigDecimalValue());
+	   	}	
+	   	
+	    blTrans.saveAccTransactionRow(transRow,id); 
+	       
+	        
+	        msg.setMessage("Ba?ar?yla Kaydedildi!");
+	        msg.open();
+	        newForm();
+	    }
+	    catch(Exception ex){
+	        ex.printStackTrace();
+	        
+	    }
+	    
+	}
+	public void delete(){
+	    
+	}
+	public void newForm(){
+	    
 	}
 
 }
