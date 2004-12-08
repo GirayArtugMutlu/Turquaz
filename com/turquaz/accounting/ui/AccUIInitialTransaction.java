7e627144f936bb48a42c9477e9c2f8ee87191e71
cemdayanik <>
2004-12-08 15:32:12 +0000

    *** empty log message ***

--- com/turquaz/accounting/ui/AccUIInitialTransaction.java
+++ com/turquaz/accounting/ui/AccUIInitialTransaction.java
@@ -43,6 +43,7 @@ import org.eclipse.swt.SWT;
 * for any corporate or commercial purpose.
 * *************************************
 */
+
 public class AccUIInitialTransaction extends org.eclipse.swt.widgets.Composite implements SecureComposite {
 
     {
