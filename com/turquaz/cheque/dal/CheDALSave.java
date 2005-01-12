6b52e14ae0ddd96cb498b7da6b1807f32e654727
onsel <>
2005-01-12 10:41:13 +0000

    Cheque Module initialized

---
+++ com/turquaz/cheque/dal/CheDALSave.java
@@ -0,0 +1,71 @@
+
+package com.turquaz.cheque.dal;
+
+import net.sf.hibernate.Session;
+import net.sf.hibernate.Transaction;
+
+import com.turquaz.engine.dal.EngDALSessionFactory;
+
+public class CheDALSave {
+  
+    public static void save(Object obj)throws Exception {
+        try{
+            Session session = EngDALSessionFactory.openSession();
+            Transaction tx = session.beginTransaction();
+            session.save(obj);
+            
+            tx.commit();
+            session.flush();
+            session.close();
+            
+            
+            
+            
+        }
+        catch(Exception ex){
+            throw ex;
+        }
+        
+    }
+    
+    public static void update(Object obj)throws Exception {
+        try{
+            
+            Session session = EngDALSessionFactory.openSession();
+            Transaction tx = session.beginTransaction();
+            session.update(obj);
+            
+            tx.commit();
+            session.flush();
+            session.close();
+            
+            
+            
+        }
+        catch(Exception ex){
+            throw ex;
+        }
+        
+        
+        
+    }
+    
+    public static void delete(Object obj)throws Exception{
+        try{
+            
+            Session session = EngDALSessionFactory.openSession();
+            Transaction tx = session.beginTransaction();
+            session.delete(obj);
+            
+            tx.commit();
+            session.flush();
+            session.close();
+            
+            
+            
+        }
+        catch(Exception ex){
+            throw ex;
+        }
+    } 
+}
