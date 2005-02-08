a72ae7f2f1cdad92417ae6777b3bd478e930d84d
onsel <>
2005-02-08 14:17:06 +0000

    linux and windows jar description files added.. linuz and windows MANIFEST.MF files added..

--- com/turquaz/inventory/ui/InvUIInventoryLedger.java
+++ com/turquaz/inventory/ui/InvUIInventoryLedger.java
@@ -22,20 +22,6 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.SWT;
 
 
-/**
-* This code was generated using CloudGarden's Jigloo
-* SWT/Swing GUI Builder, which is free for non-commercial
-* use. If Jigloo is being used commercially (ie, by a corporation,
-* company or business for any purpose whatever) then you
-* should purchase a license for each developer using Jigloo.
-* Please visit www.cloudgarden.com for details.
-* Use of Jigloo implies acceptance of these licensing terms.
-* *************************************
-* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED
-* for this machine, so Jigloo or this code cannot be used legally
-* for any corporate or commercial purpose.
-* *************************************
-*/
 public class InvUIInventoryLedger extends org.eclipse.swt.widgets.Composite implements SearchComposite{
 	private Composite compFilter;
 	private TableColumn tableColumnInvCode;
@@ -64,7 +50,7 @@ public class InvUIInventoryLedger extends org.eclipse.swt.widgets.Composite impl
 	private void initGUI() {
 		try {
 			this.setLayout(new GridLayout());
-			this.setSize(545, 281);
+			this.setSize(592, 281);
             {
                 compFilter = new Composite(this, SWT.NONE);
                 GridLayout compFilterLayout = new GridLayout();
