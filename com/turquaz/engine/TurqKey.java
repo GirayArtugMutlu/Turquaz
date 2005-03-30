20eabdf1ffd8294156ebb6af34d8c4c77a695ff9
onsel <>
2005-03-30 09:12:37 +0000

    *** empty log message ***

---
+++ com/turquaz/engine/TurqKey.java
@@ -0,0 +1,18 @@
+/*
+ * Created on Mar 30, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.turquaz.engine;
+
+/**
+ * @author onsel
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class TurqKey
+{	
+	
+}
