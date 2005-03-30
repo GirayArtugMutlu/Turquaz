1db8f9f7674d5b48cb3c25837b25a11d5625d95d
onsel <>
2005-03-30 15:04:54 +0000

    *** empty log message ***

---
+++ com/turquaz/accounting/AccKeys.java
@@ -0,0 +1,45 @@
+/*
+ * Created on Mar 30, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.turquaz.accounting;
+
+/**
+ * @author onsel
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class AccKeys
+{
+	public final static String ACC_PARENT_ID = "parentId";
+	public final static String ACC_CODE_CRITERIA = "codeCriteria";
+	public final static String ACC_ACCOUNT_NAME = "accountName";
+	public final static String ACC_ACCOUNT_CODE = "accountCode";
+	public final static String ACC_ACCOUNT = "account";
+	public final static String ACC_PARENT_ACCOUNT = "parentAccount";
+	public final static String ACC_TYPE ="type";
+	public final static String ACC_START_DATE = "startDate";
+	public final static String ACC_END_DATE = "endDate";
+	public final static String ACC_TRANS_ROW = "transRow";
+	public final static String ACC_TRANS_ID = "transId";
+	public final static String ACC_TRANS_DATE = "transDate";
+	public final static String ACC_DOCUMENT_NO = "documentNo";
+	public final static String ACC_MODULE_ID = "moduleId";
+	public final static String ACC_SEQUENCE_ID ="sequenceId";
+	public final static String ACC_DEFINITION = "definition";
+	public final static String ACC_CREDIT_ACCOUNT_MAP =	"creditAccountMap";
+	public final static String ACC_DEPT_ACCOUNT_MAP =	"deptAccountMap";
+	public final static String ACC_SUM_ROWS = "sumRows";
+	public final static String ACC_IS_GENERAL ="isGeneral";
+    public final static String ACC_IS_PAYMENT = "isPayment";
+    public final static String ACC_IS_COLLECT ="isCollect";
+    public final static String ACC_ACCOUNT_START ="accountStart";
+    public final static String ACC_ACCOUNT_END ="accountEnd";
+    public final static String ACC_TRANSACTION ="accTransaction";
+   
+	
+	
+}
