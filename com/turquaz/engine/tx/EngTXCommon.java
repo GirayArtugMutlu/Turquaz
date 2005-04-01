51ca1e0ebcee09750ad3ee9122660045c840bbe8
onsel <>
2005-04-01 08:53:54 +0000

    *** empty log message ***

--- com/turquaz/engine/tx/EngTXCommon.java
+++ com/turquaz/engine/tx/EngTXCommon.java
@@ -23,41 +23,10 @@ public class EngTXCommon
 	public static Object doSingleTX(String myClass, String myMethod, HashMap argMap)throws Exception
 	{
 		Session session=null;
-		try
-		{			
-			session=EngDALSessionFactory.openSession2();
-			Class cls=Class.forName(myClass);
-			Class[] classList=null;
-			Object[] argList=null;
-			if (argMap != null)
-			{
-				classList=new Class[]{argMap.getClass()};
-				argList=new Object[]{argMap};
-			}
-			Method method=cls.getMethod(myMethod,classList);
-			Object retVal=method.invoke(null,argList);
-			return retVal;
-		}
-		catch(Exception ex)
-		{
-			throw ex;
-		}
-		finally
-		{
-			if (session != null)
-			{
-				session.close();
-			}
-		}
-	}
-	
-	public static Object doTransactionTX(String myClass, String myMethod, HashMap argMap)throws Exception
-	{
-		Session session=null;
 		Transaction tx=null;
 		try
 		{			
-			session=EngDALSessionFactory.openSession2();
+			session=EngDALSessionFactory.openSession();	
 			tx=session.beginTransaction();
 			Class cls=Class.forName(myClass);
 			Class[] classList=null;
@@ -75,13 +44,53 @@ public class EngTXCommon
 		catch(Exception ex)
 		{
 			if (tx != null)
+			{
 				tx.rollback();
+			}
 			throw ex;
 		}
 		finally
 		{
 			if (session != null)
 			{
+				session.close();
+			}
+		}
+	}
+	
+	public static Object doTransactionTX(String myClass, String myMethod, HashMap argMap)throws Exception
+	{
+		Session session=null;
+		Transaction tx=null;
+		try
+		{			
+			session=EngDALSessionFactory.openSession();
+			tx=session.beginTransaction();
+			Class cls=Class.forName(myClass);
+			Class[] classList=null;
+			Object[] argList=null;
+			if (argMap != null)
+			{
+				classList=new Class[]{argMap.getClass()};
+				argList=new Object[]{argMap};
+			}
+			Method method=cls.getMethod(myMethod,classList);
+			Object retVal=method.invoke(null,argList);
+			tx.commit();
+			return retVal;
+		}
+		catch(Exception ex)
+		{
+			if (tx != null){
+				tx.rollback();
+			}
+			throw ex;
+		}
+		finally
+		{
+			if (session != null)
+			{
+				
 				session.close();
 			}
 		}
