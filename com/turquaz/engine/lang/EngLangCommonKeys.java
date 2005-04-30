30e34fabe115c933fe9c8c45871ff21cc407c06f
huseyiner <>
2005-04-30 14:55:39 +0000

    language cleaning cont.

--- com/turquaz/engine/lang/EngLangCommonKeys.java
+++ com/turquaz/engine/lang/EngLangCommonKeys.java
@@ -11,6 +11,7 @@ public class EngLangCommonKeys {
 	public final static String STR_DATE = "Tarih";
 	public final static String STR_TOTAL = "Toplam";
 	public final static String STR_DESCRIPTION = "Açýklama";
+	public final static String STR_OK = "Tamam";
 	
 	
 	public final static String STR_EXCHANGE="Kur";
