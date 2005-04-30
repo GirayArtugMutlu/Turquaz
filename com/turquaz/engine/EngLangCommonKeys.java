f62d2c57cc7a0ccbf27e7b91bf7e484c5a157a67
huseyiner <>
2005-04-30 11:04:56 +0000

    externalization and reformat of language keys started

---
+++ com/turquaz/engine/EngLangCommonKeys.java
@@ -0,0 +1,33 @@
+
+package com.turquaz.engine;
+
+
+public class EngLangCommonKeys {
+
+	public final static String STR_UPDATE = "Güncelle";
+	public final static String STR_DELETE = "Sil";
+	public final static String STR_SAVE = "Kaydet";
+	public final static String STR_CANCEL = "Ýptal";
+	public final static String STR_DATE = "Tarih";
+	public final static String STR_DESCRIPTION = "Açýklama";
+	public final static String STR_CURRENCY = "Para Birimi";
+	public final static String STR_MONTHS_JANUARY = "Ocak";
+	public final static String STR_MONTHS_FEBRUARY = "Þubat";
+	public final static String STR_MONTHS_MARCH = "Mart";
+	public final static String STR_MONTHS_APRIL = "Nisan";
+	public final static String STR_MONTHS_MAY = "Mayýs";
+	public final static String STR_MONTHS_JUNE = "Haziran";
+	public final static String STR_MONTHS_JULY = "Temmuz";
+	public final static String STR_MONTHS_AUGUST = "Aðustos";
+	public final static String STR_MONTHS_SEPTEMBER = "Eylül";
+	public final static String STR_MONTHS_OCTOBER = "Ekim";
+	public final static String STR_MONTHS_NOVEMBER = "Kasým";
+	public final static String STR_MONTHS_DECEMBER = "Aralýk";
+	
+	public final static String MSG_SAVED_SUCCESS = "Baþarýyla Kaydedildi!";
+	public final static String MSG_UPDATED_SUCCESS = "Baþarýyla Güncellendi!";
+	public final static String MSG_DELETED_SUCCESS = "Baþarýyla Silindi!";
+	public final static String MSG_DELETE_REALLY = "Silmek istediðinizden emin misiniz?";
+	public final static String MSG_UNKNOWN_ERROR_REPORT_ERROR_LOG = "Bilinmeyen bir hata oluþtu. error.log dosyasýndan eriþilebilir.";
+	
+}
