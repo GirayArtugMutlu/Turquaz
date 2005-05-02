f1c69f5aa76800a6f9c67035eccf44c78008eeec
cemdayanik <>
2005-05-02 14:41:34 +0000

    *** empty log message ***

--- com/turquaz/engine/lang/EngLangCommonKeys.java
+++ com/turquaz/engine/lang/EngLangCommonKeys.java
@@ -3,70 +3,70 @@ package com.turquaz.engine.lang;
 
 
 public class EngLangCommonKeys {
-
-	
-	public final static String STR_OPERATIONS="??lemler";
-	public final static String STR_UPDATE = "Güncelle";
-	public final static String STR_DELETE = "Sil";
-	public final static String STR_SAVE = "Kaydet";
-	public final static String STR_CANCEL = "Ýptal";
-	public final static String STR_DATE = "Tarih";
-	public final static String STR_TOTAL = "Toplam";
-	public final static String STR_TOTAL_CAPITAL="TOPLAM";
-	public final static String STR_GENERAL_TOTAL_CAPITAL="GENEL TOPLAM";
-	public final static String STR_DESCRIPTION = "Açýklama";
-	public final static String STR_OK = "Tamam";
-	public final static String STR_PRINT = "Yazdýr";
-	public final static String STR_AMOUNT="Miktar";
-	public final static String STR_TYPE="Tipi";
-	public final static String STR_START_DATE="Ba?lang?ç Tarihi";
-	public final static String STR_END_DATE="Biti? Tarihi";
-	public final static String STR_SEARCH_RESULT="Arama Sonucu";
-	public final static String STR_REPORT="Rapor";
-    public final static String STR_TOTAL_CREDIT="Toplama Alacak";
-    public final static String STR_TOTAL_DEPT = "Toplam Borç";
-    public final static String STR_BALANCE = "Bakiye";
-    public final static String STR_BALANCE_CREDIT ="Alacak Bakiye";
-    public final static String STR_BALANCE_DEPT =  "Borç Bakiye";
-	
-	public final static String STR_EXCHANGE="Kur";
-	public final static String STR_EXCHANGE_RATE="Kur Oran?";
-    public final static String STR_CURRENCY_ABBR="Para Birimi Kýsaltmasý";
-    public final static String STR_CURRENCY_COUNTRY ="Ülke";
-	
-	public final static String STR_CURRENCY = "Para Birimi";
-	public final static String STR_MONTHS_JANUARY = "Ocak";
-	public final static String STR_MONTHS_FEBRUARY = "Þubat";
-	public final static String STR_MONTHS_MARCH = "Mart";
-	public final static String STR_MONTHS_APRIL = "Nisan";
-	public final static String STR_MONTHS_MAY = "Mayýs";
-	public final static String STR_MONTHS_JUNE = "Haziran";
-	public final static String STR_MONTHS_JULY = "Temmuz";
-	public final static String STR_MONTHS_AUGUST = "Aðustos";
-	public final static String STR_MONTHS_SEPTEMBER = "Eylül";
-	public final static String STR_MONTHS_OCTOBER = "Ekim";
-	public final static String STR_MONTHS_NOVEMBER = "Kasým";
-	public final static String STR_MONTHS_DECEMBER = "Aralýk";
-	
-	public final static String STR_GENERAL_INFO="Genel Bilgi";
-	public final static String STR_DETAILS="Ayr?nt?lar";
-	public final static String STR_UNITS="Birimler";
-	public final static String STR_GROUPS="Gruplar";
-    public final static String STR_GROUP_NAME="Grup Adý";
-    public final static String STR_DOCUMENT_NO="Belge No";
+	public final static String MSG_DELETE_REALLY = "Silmek istediðinizden emin misiniz?";
+	public final static String MSG_DELETED_SUCCESS = "Baþarýyla Silindi!";
 	
 	public final static String MSG_SAVED_SUCCESS = "Baþarýyla Kaydedildi!";
-	public final static String MSG_UPDATED_SUCCESS = "Baþarýyla Güncellendi!";
-	public final static String MSG_DELETED_SUCCESS = "Baþarýyla Silindi!";
-	public final static String MSG_DELETE_REALLY = "Silmek istediðinizden emin misiniz?";
 	public final static String MSG_UNKNOWN_ERROR_REPORT_ERROR_LOG = "Bilinmeyen bir hata oluþtu. error.log dosyasýndan eriþilebilir.";
-	public final static String STR_UNIT="Birim";
-	public final static String STR_UNIT_NAME="Birim Ad?";
+	public final static String MSG_UPDATED_SUCCESS = "Baþarýyla Güncellendi!";
 	public final static String STR_ADD="Ekle";
 	
 	public final static String STR_ADDRESS="Adres";
+	public final static String STR_AMOUNT="Miktar";
+    public final static String STR_BALANCE = "Bakiye";
+    public final static String STR_BALANCE_CREDIT ="Alacak Bakiye";
+    public final static String STR_BALANCE_DEPT =  "Borç Bakiye";
+	public final static String STR_CANCEL = "Ýptal";
 	public final static String STR_CITY="?ehir";
+	
+	public final static String STR_CURRENCY = "Para Birimi";
+    public final static String STR_CURRENCY_ABBR="Para Birimi Kýsaltmasý";
+    public final static String STR_CURRENCY_COUNTRY ="Ülke";
+	public final static String STR_DATE = "Tarih";
+	public final static String STR_DELETE = "Sil";
+	public final static String STR_DESCRIPTION = "Açýklama";
+	public final static String STR_DETAILS="Ayr?nt?lar";
+    public final static String STR_DOCUMENT_NO="Belge No";
+	public final static String STR_END_DATE="Biti? Tarihi";
+	
+	public final static String STR_EXCHANGE="Kur";
+	public final static String STR_EXCHANGE_RATE="Kur Oran?";
+	
+	public final static String STR_GENERAL_INFO="Genel Bilgi";
+	public final static String STR_GENERAL_TOTAL_CAPITAL="GENEL TOPLAM";
+    public final static String STR_GROUP_NAME="Grup Adý";
+	public final static String STR_GROUPS="Gruplar";
+	public final static String STR_MONTHS_APRIL = "Nisan";
+	public final static String STR_MONTHS_AUGUST = "Aðustos";
+	public final static String STR_MONTHS_DECEMBER = "Aralýk";
+	public final static String STR_MONTHS_FEBRUARY = "Þubat";
+	public final static String STR_MONTHS_JANUARY = "Ocak";
+	public final static String STR_MONTHS_JULY = "Temmuz";
+	public final static String STR_MONTHS_JUNE = "Haziran";
+	public final static String STR_MONTHS_MARCH = "Mart";
+	public final static String STR_MONTHS_MAY = "Mayýs";
+	public final static String STR_MONTHS_NOVEMBER = "Kasým";
+	public final static String STR_MONTHS_OCTOBER = "Ekim";
+	public final static String STR_MONTHS_SEPTEMBER = "Eylül";
+	public final static String STR_OK = "Tamam";
+
+	
+	public final static String STR_OPERATIONS="??lemler";
+	public final static String STR_PRINT = "Yazdýr";
+	public final static String STR_REPORT="Rapor";
+	public final static String STR_SAVE = "Kaydet";
+	public final static String STR_SEARCH_RESULT="Arama Sonucu";
+	public final static String STR_START_DATE="Ba?lang?ç Tarihi";
 	public final static String STR_TELEPHONE="Telefon";
+	public final static String STR_TOTAL = "Toplam";
+	public final static String STR_TOTAL_CAPITAL="TOPLAM";
+    public final static String STR_TOTAL_CREDIT="Toplama Alacak";
+    public final static String STR_TOTAL_DEPT = "Toplam Borç";
+	public final static String STR_TYPE="Tipi";
+	public final static String STR_UNIT="Birim";
+	public final static String STR_UNIT_NAME="Birim Ad?";
+	public final static String STR_UNITS="Birimler";
+	public final static String STR_UPDATE = "Güncelle";
 	
 	
 	
--- com/turquaz/engine/lang/InvLangKeys.java
+++ com/turquaz/engine/lang/InvLangKeys.java
@@ -14,107 +14,108 @@ package com.turquaz.engine.lang;
  */
 public class InvLangKeys
 {
-	public final static String TITLE_INV_CARD_SEARCH="Stok Kart? Arama";
-	public final static String TITLE_INV_CARD_UPDATE="Stok Kart? Güncelleme";
-	public final static String TITLE_INV_SUB_GROUP_ADD="Stok Alt Grubu Ekleme";
-	public final static String TITLE_INV_GROUPS="Stok Gruplar?";
-	public final static String TITLE_INV_UNITS="Stok Birimleri";
-	public final static String TITLE_WAREHOUSE_UPDATE="Depo Güncelleme";
-	
-	public final static String STR_INV_LEDGER="Envanter Listesi";
-	
-
-	public final static String STR_INV_CODE="Stok Kodu";
-	public final static String STR_INV_NAME="Stok Ad?";
-	public final static String STR_MIN_AMOUNT="Minumum Miktar";
-	public final static String STR_MAX_AMOUNT="Maksimum Miktar";
-	public final static String STR_VAT="K.D.V";
-	public final static String STR_DISCOUNT="?ndirim";
-	public final static String STR_SPEC_VAT_PERC="ÖTV%";
-	public final static String STR_SPEC_VAT_EACH="Birim ÖTV Tutar?";
-	public final static String STR_BASE_UNIT="Temel Birim";
-	public final static String STR_SUB_UNITS="Alt Birimler";
-	public final static String STR_UPDATE_UNITS="Birimleri Güncelle";
-	public final static String STR_UNITS="Birimler";
-	public final static String STR_INV_UNITS="Stok Birimleri";
-	public final static String STR_COEFFICIENT="Katsay?";
-	public final static String STR_PRICE_TYPE="Fiyat Tipi";
-	public final static String STR_TOTALPRICE="Tutar";
-	public final static String STR_INV_CARD="Stok Kart?";
-	public final static String STR_INV_CARD_START="Stok Kart? - Ba?lang?ç";
-	public final static String STR_INV_CARD_END="Stok Kart? - Biti?";
-	public final static String STR_INV_CODE_START="Stok Kodu - Ba?lang?ç";
-	public final static String STR_INV_CODE_END="Stok Kodu - Biti?";
-	public final static String STR_INV_NAME_START="Stok Ad? - Ba?lang?ç";
-	public final static String STR_INV_NAME_END="Stok Ad? - Biti?";
-	public final static String STR_INV_TRANSACTIONS="Stok Hareketleri";
-	public final static String STR_MAIN_GROUP="Ana Grup";
-	public final static String STR_FILTER="Filtre";
-	public final static String STR_ALL_INV="Bütün Stoklar";
-	public final static String STR_HAS_TRANSACTIONS="Hareketliler";
-	public final static String STR_HAS_BALANCE="Bakiyeliler";
-	public final static String STR_LAST_AMOUNT="Son Miktar";
-	public final static String STR_AVERAGE_PRICE="Ort. Fiyat";
-	public final static String STR_TOTAL_PRICE="Toplam Tutar";
-	
-	public final static String STR_INITIAL_TRANSACTION="Aç?l?? Hareketi";
-	
-	public final static String STR_WAREHOUSE_CODE="Depo Kodu";
-	public final static String STR_WAREHOUSE_NAME="Depo Ad?";
-	
-	public final static String STR_SEARCH_BY_CODE="Koda Göre Ara";
-	public final static String STR_SEARCH_BY_NAME="Ada Göre Ara";
-	
-	public final static String STR_ACCOUNTING_TYPE="Muhasebe Tipi";
-	
-	public final static String STR_ACCOUNTING_CODES="Muhasebe Kodlar?";
-	public final static String STR_PRICE_INFO="Fiyat Bilgileri";
-	
-	public final static String STR_MAIN_GROUPS="Ana Gruplar";
-	public final static String STR_SUB_GROUPS="Alt Gruplar";
-	public final static String STR_INV_MAIN_GROUP="Stok Ana Grup";
-	public final static String STR_INV_SUB_GROUP="Stok Alt Grup";
-	public final static String STR_INV_CARDS="Stok Kartlar?";
-	public final static String STR_INV_GROUPS="Stok Gruplar?";
-	public final static String STR_NEW_SUB_GROUP="Yeni Alt Grup";
-	public final static String STR_WAREHOUSE="Depo";
+	public final static String MSG_ENTER_AMOUNT="Lütfen miktar giriniz!";
+	public final static String MSG_ENTER_GROUP_NAME="Lütfen grup ad? giriniz!";
 	public final static String MSG_ENTER_INV_CODE="Stok kodu girmelisiniz!";
 	public final static String MSG_ENTER_INV_NAME="Stok ad? girmelisiniz!";
+	public final static String MSG_ENTER_UNIT_NAME="Lütfen birim ad?n? doldurunuz!";
+	public final static String MSG_ENTER_WAREHOUSE_NAME="Lütfen depo ad?n? giriniz!";
+	
+	
+	public final static String MSG_INV_CARD_HAS_TRANSACTION="Bu stok kart? stok hareket görmü?\n?lk önce hareketleri silmeniz gerekir!";
 	public final static String MSG_NOT_ENTER_INV_CODE_ALREADY_EXIST="Önceden varolan bir stok kodu giremezsiniz!";
+	public final static String MSG_NOT_ENTER_UNIT_NAME_ALREADY_EXIST="Daha önce varolan bir birim ad? giremezsiniz!";
 	public final static String MSG_SELECT_BASE_UNIT="Temel birim seçmelisiniz!";
+	public final static String MSG_SELECT_INV_CARD="Lütfen stok kart? seçiniz!";
+	public final static String MSG_WAREHOUSE_HAS_TRANSACTION="Bu depo hareket görmü?\n?lk önce onlar? silmeniz gerekir!";
+	
+	
+	public final static String STR_ALL_INV="Bütün Stoklar";
 	
 	public final static String STR_AMOUNT_IN="Giri? Miktar?";
 	public final static String STR_AMOUNT_OUT="Çk?? Miktar?";
+	public final static String STR_AVERAGE_PRICE="Ort. Fiyat";
+	public final static String STR_AVERAGE_PRICE_IN="Al?? Fiyat? (Ort.)";
+	public final static String STR_BALANCE_AMOUNT="Bakiye Miktar?";
 	public final static String STR_BALANCE_AMOUNT_IN="Bakiye Giri?";
 	public final static String STR_BALANCE_AMOUNT_OUT="Bakiye Ç?k??";
-	public final static String STR_PRICE_IN="Giri? Tutar?";
-	public final static String STR_PRICE_OUT="Ç?k?? Tutar?";
-	public final static String STR_BALANCE_AMOUNT="Bakiye Miktar?";
 	public final static String STR_BALANCE_PRICE="Bakiye Tutar?";
-	public final static String STR_UNIT_PRICE_IN="Gir. Birim Fiyat?";
-	public final static String STR_UNIT_PRICE_OUT="Ç?k. Birim Fiyat?";
-	public final static String STR_AVERAGE_PRICE_IN="Al?? Fiyat? (Ort.)";
-	public final static String STR_SELL_COST="Sat?? Maliyeti";
-	public final static String STR_SELL_TOTALPRICE="Sat?? Tutar?";
-	public final static String STR_SELL_AMOUNT="Sat?? Miktar?";
+	public final static String STR_BALANCE_UNIT_PRICE="Bak. Birim Fiyat?";
+	public final static String STR_BASE_UNIT="Temel Birim";
+	public final static String STR_COEFFICIENT="Katsay?";
+	public final static String STR_DISCOUNT="?ndirim";
+	public final static String STR_FILTER="Filtre";
+	public final static String STR_HAS_BALANCE="Bakiyeliler";
+	public final static String STR_HAS_TRANSACTIONS="Hareketliler";
+	
+	public final static String STR_INITIAL_TRANSACTION="Aç?l?? Hareketi";
+	public final static String STR_INV_CARD="Stok Kart?";
+	public final static String STR_INV_CARD_END="Stok Kart? - Biti?";
+	public final static String STR_INV_CARD_START="Stok Kart? - Ba?lang?ç";
+	public final static String STR_INV_CARDS="Stok Kartlar?";
+	
+
+	public final static String STR_INV_CODE="Stok Kodu";
+	public final static String STR_INV_CODE_END="Stok Kodu - Biti?";
+	public final static String STR_INV_CODE_START="Stok Kodu - Ba?lang?ç";
+	public final static String STR_INV_GROUPS="Stok Gruplar?";
+	
+	public final static String STR_INV_LEDGER="Envanter Listesi";
+	public final static String STR_INV_MAIN_GROUP="Stok Ana Grup";
+	public final static String STR_INV_NAME="Stok Ad?";
+	public final static String STR_INV_NAME_END="Stok Ad? - Biti?";
+	public final static String STR_INV_NAME_START="Stok Ad? - Ba?lang?ç";
+	public final static String STR_INV_SUB_GROUP="Stok Alt Grup";
+	public final static String STR_INV_TRANSACTIONS="Stok Hareketleri";
+	public final static String STR_INV_UNITS="Stok Birimleri";
+	public final static String STR_LAST_AMOUNT="Son Miktar";
+	public final static String STR_MAIN_GROUP="Ana Grup";
+	
+	public final static String STR_MAIN_GROUPS="Ana Gruplar";
+	public final static String STR_MAX_AMOUNT="Maksimum Miktar";
+	public final static String STR_MIN_AMOUNT="Minumum Miktar";
+	public final static String STR_NEW_SUB_GROUP="Yeni Alt Grup";
+	public final static String STR_PRICE_IN="Giri? Tutar?";
+	
+	public final static String STR_PRICE_INFO="Fiyat Bilgileri";
+	public final static String STR_PRICE_OUT="Ç?k?? Tutar?";
+	public final static String STR_PRICE_TYPE="Fiyat Tipi";
 	public final static String STR_PROFIT_ABBR="K/Z";
 	public final static String STR_PROFIT_ANALYSIS="Kar-Zarar Analizi";
+	
+	public final static String STR_SEARCH_BY_CODE="Koda Göre Ara";
+	public final static String STR_SEARCH_BY_NAME="Ada Göre Ara";
+	public final static String STR_SELL_AMOUNT="Sat?? Miktar?";
+	public final static String STR_SELL_COST="Sat?? Maliyeti";
+	public final static String STR_SELL_TOTALPRICE="Sat?? Tutar?";
+	public final static String STR_SPEC_VAT_EACH="Birim ÖTV Tutar?";
+	public final static String STR_SPEC_VAT_PERC="ÖTV%";
+	public final static String STR_SUB_GROUPS="Alt Gruplar";
+	public final static String STR_SUB_UNITS="Alt Birimler";
+	public final static String STR_TOTAL_PRICE="Toplam Tutar";
+	public final static String STR_TOTALPRICE="Tutar";
 	
 	public final static String STR_TRANSOVER_AMOUNT="Devir Mik.";
 	public final static String STR_TRANSOVER_PRICE="Devir Tutar?";
 	public final static String STR_TRANSOVER_UNIT_PRICE="Devir Bir. Fiyat?";
-	public final static String STR_BALANCE_UNIT_PRICE="Bak. Birim Fiyat?";
+	public final static String STR_UNIT_PRICE_IN="Gir. Birim Fiyat?";
+	public final static String STR_UNIT_PRICE_OUT="Ç?k. Birim Fiyat?";
+	public final static String STR_UNITS="Birimler";
+	public final static String STR_UPDATE_UNITS="Birimleri Güncelle";
+	public final static String STR_VAT="K.D.V";
+	public final static String STR_WAREHOUSE="Depo";
+	
+	public final static String STR_WAREHOUSE_CODE="Depo Kodu";
+	public final static String STR_WAREHOUSE_NAME="Depo Ad?";
 	public final static String STR_WAREHOUSES="Depolar";
 	
 	
-	public final static String MSG_INV_CARD_HAS_TRANSACTION="Bu stok kart? stok hareket görmü?\n?lk önce hareketleri silmeniz gerekir!";
-	public final static String MSG_ENTER_GROUP_NAME="Lütfen grup ad? giriniz!";
-	public final static String MSG_SELECT_INV_CARD="Lütfen stok kart? seçiniz!";
-	public final static String MSG_ENTER_AMOUNT="Lütfen miktar giriniz!";
-	public final static String MSG_ENTER_UNIT_NAME="Lütfen birim ad?n? doldurunuz!";
-	public final static String MSG_NOT_ENTER_UNIT_NAME_ALREADY_EXIST="Daha önce varolan bir birim ad? giremezsiniz!";
-	public final static String MSG_ENTER_WAREHOUSE_NAME="Lütfen depo ad?n? giriniz!";
-	public final static String MSG_WAREHOUSE_HAS_TRANSACTION="Bu depo hareket görmü?\n?lk önce onlar? silmeniz gerekir!";
+	public final static String TITLE_INV_CARD_SEARCH="Stok Kart? Arama";
+	public final static String TITLE_INV_CARD_UPDATE="Stok Kart? Güncelleme";
+	public final static String TITLE_INV_GROUPS="Stok Gruplar?";
+	public final static String TITLE_INV_SUB_GROUP_ADD="Stok Alt Grubu Ekleme";
+	public final static String TITLE_INV_UNITS="Stok Birimleri";
+	public final static String TITLE_WAREHOUSE_UPDATE="Depo Güncelleme";
 	
 	
 	
