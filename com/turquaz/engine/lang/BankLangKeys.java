f3ca96cadad3cd1ee268ddf52cfea29c1c65bcd4
huseyiner <>
2005-05-02 15:07:26 +0000

    *** empty log message ***

---
+++ com/turquaz/engine/lang/BankLangKeys.java
@@ -0,0 +1,27 @@
+
+package com.turquaz.engine.lang;
+
+public class BankLangKeys {
+
+	public static String STR_TRANFER = "Banka Virman- ";
+    public static String STR_INITIAL_TRANSACTION = "Açýlýþ Deðeri";
+    public static String STR_TRANSFER_FROM = " 'den havale";
+    public static String STR_TRANSFER_TO = " ''e havale";
+    public static String STR_BANK_CARD = "Banka Kartý";
+    public static String STR_START_DATE = "Baþlangýç Tarihi";
+    public static String STR_END_DATE = "Bitiþ Tarihi";
+    public static String STR_TRANSACTION_TYPE = "Hareket Tipi";
+    public static String STR_DEBIT = "Borç";
+    public static String STR_CREDIT = "Alacak";
+    public static String STR_BALANCE_DEBIT = "Bakiye Borç";
+    public static String STR_BALANCE_CREDIT = "Bakiye Alacak";
+    public static String STR_REPORT = "Rapor";
+    public static String STR_BANK_ABSTRACT = "Banka Kartý Ekstresi";
+    public static String STR_TURNOVER = "DEVREDEN";
+    public static String STR_TURNOVER_TAGS = "--DEVREDEN--";
+    public static String STR_TURNOVER_TOTAL_TAGS = "--DÖNEM YEKÜNÜ--";
+    public static String STR_TOTAL_TAGS = "--TOTAL--";
+    public static String STR_BANK_CODE = "Banka Kodu";
+    
+    public static String MSG_PLEASE_SELECT_BANK_CARD = "Lütfen Banka Kartý Seçiniz!";
+}
