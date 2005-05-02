973c40173a5d69be5e5c5bc944eb2e1f44fc1d20
cemdayanik <>
2005-05-02 08:09:18 +0000

    *** empty log message ***

--- com/turquaz/engine/lang/CurLangKeys.java
+++ com/turquaz/engine/lang/CurLangKeys.java
@@ -3,8 +3,9 @@ package com.turquaz.engine.lang;
 public class CurLangKeys
 {
 
-    public static String STR_CUR_CARD_START = "Cari Kart - Baþlangýç";
-    public static String STR_CUR_CARD_END = "Cari Kart - Bitiþ";
+    public final static String STR_CUR_CARD_START = "Cari Kart - Baþlangýç";
+    public final static String STR_CUR_CARD_END = "Cari Kart - Bitiþ";
+    public final static String STR_CUR_CARD="Cari Kart";
 
 
 }
