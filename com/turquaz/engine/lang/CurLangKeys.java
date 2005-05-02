2052a8dfae7e0efbbcabfa1bfaf4fca7130925f7
onsel <>
2005-05-02 10:44:28 +0000

    *** empty log message ***

--- com/turquaz/engine/lang/CurLangKeys.java
+++ com/turquaz/engine/lang/CurLangKeys.java
@@ -6,6 +6,4 @@ public class CurLangKeys
     public final static String STR_CUR_CARD_START = "Cari Kart - Baþlangýç";
     public final static String STR_CUR_CARD_END = "Cari Kart - Bitiþ";
     public final static String STR_CUR_CARD="Cari Kart";
-
-
 }
