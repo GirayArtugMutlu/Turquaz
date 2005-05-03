47972bbae76289b52b9ad389c36890155d4d7255
onsel <>
2005-05-03 13:57:44 +0000

    *** empty log message ***

---
+++ com/turquaz/engine/lang/CheLangKeys.java
@@ -0,0 +1,35 @@
+package com.turquaz.engine.lang;
+
+public class CheLangKeys
+{
+
+    public static String TITLE_CHEQUES_IN_BANK = "Bankadaki Çekler";
+    public static String TITLE_GIVEN_CHEQUES = "Ciro Edilen Çekler";
+    public static String TITLE_CUSTOMER_CHEQUE_ADD ="Müþteri Çeki Ekleme";
+    
+   public static String STR_CHEQUE_INFO = "Çek Bilgileri";
+   public static String STR_PORTFOLIO_NO ="Portföy No";
+   public static String STR_CHEQUE_NO ="Çek Numarasý";
+   public static String STR_DUE_DATE = "Vade Tarihi";
+   public static String STR_CHEQUE_HISTORY ="Çek Tarihçesi";
+   public static String STR_ROLL_NO ="Bordro No";
+   public static String STR_ROLL_TYPE = "Bordro Tipi";
+   public static String STR_BANK_CURRENT_CODE ="Cari/Banka Kodu";
+   public static String STR_ROLL_DATE ="Bordro Tarihi";
+   public static String STR_PAYMENT_PLACE ="Ödeme Yeri";
+   public static String STR_DEPTOR = "Borçlu";
+   public static String STR_SUM_TOTALS = "Cari Hareketleri Topla";
+   public static String STR_CUSTOMER_CHEQUE = "Müþteri Çeki";
+   public static String STR_OWN_CHEQUE = "Firma Çeki";
+   public static String STR_ROLL_OWNER = "Bordro Sahibi";
+   public static String STR_ROLLS = "Cheque Bordrolarý";
+   public static String STR_BANK_NAME = "Banka Adý";
+   public static String STR_BANK_BRANCH ="Banka Þubesi";
+   public static String STR_BANK_ACCOUNT_NO ="Hesap Numarasý";
+   
+   
+   
+   public static String MSG_ENTER_AT_LEAST_ONE_CHEQUE = "En az bir tane çek girmeniz gerekir!";
+   public static String MSG_CANNOT_DELETE_ROLLS_WITH_CHEQUE ="Çek Bulunan Bordrolarý Silemezsiniz!";
+
+}
