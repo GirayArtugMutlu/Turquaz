149948a27f33bf0c18cffd54c82de374dd1c9b8c
huseyiner <>
2005-06-21 06:28:29 +0000

    warehouse

--- com/turquaz/inventory/bl/InvBLWarehouseSearch.java
+++ com/turquaz/inventory/bl/InvBLWarehouseSearch.java
@@ -2,6 +2,9 @@ package com.turquaz.inventory.bl;
 
 import java.util.HashMap;
 import java.util.List;
+
+import com.turquaz.common.HashBag;
+import com.turquaz.engine.dal.TurqInventoryWarehous;
 import com.turquaz.inventory.InvKeys;
 import com.turquaz.inventory.dal.InvDALWarehouseSearch;
 
@@ -26,18 +29,38 @@ import com.turquaz.inventory.dal.InvDALWarehouseSearch;
  */
 public class InvBLWarehouseSearch
 {
-	public static List searchWarehouse(HashMap argMap) throws Exception
+	public static HashBag searchWarehouse(HashMap argMap) throws Exception
 	{
+        HashBag returnBag = new HashBag();
+        TurqInventoryWarehous warehouse;
+        List result;
+        
 		try
 		{
 			String name=(String)argMap.get(InvKeys.INV_WAREHOUSE_NAME);
 			String city=(String)argMap.get(InvKeys.INV_WAREHOUSE_CITY);
-			return InvDALWarehouseSearch.searchWarehouse(name, city);
-		}
-		catch (Exception ex)
-		{
-			throw ex;
-		}
+			result = InvDALWarehouseSearch.searchWarehouse(name, city);            
+     
+        for (int i = 0; i < result.size(); i++)
+        {
+            warehouse = (TurqInventoryWarehous) result.get(i);
+            
+            returnBag.put(InvKeys.INV_WAREHOUSES,i,InvKeys.INV_WAREHOUSE_CODE , warehouse.getWarehousesCode());
+            returnBag.put(InvKeys.INV_WAREHOUSES,i,InvKeys.INV_WAREHOUSE_NAME , warehouse.getWarehousesName());
+            returnBag.put(InvKeys.INV_WAREHOUSES,i,InvKeys.INV_WAREHOUSE_CITY , warehouse.getWarehousesCity());
+            returnBag.put(InvKeys.INV_WAREHOUSES,i,InvKeys.INV_WAREHOUSE_TEL , warehouse.getWarehousesTelephone());
+            returnBag.put(InvKeys.INV_WAREHOUSES,i,InvKeys.INV_WAREHOUSE_ID , warehouse.getId());
+            returnBag.put(InvKeys.INV_WAREHOUSES,i,InvKeys.INV_WAREHOUSE_DESC , warehouse.getWarehousesDescription());
+            
+        }
+        
+        
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+        return returnBag;
 	}
 
 	public static List getInventoryWarehouses() throws Exception
